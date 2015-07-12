917ad9cbfff1152e8256b05148ec95bb57c3f1c6
ghostalex <alexanderml_12@hotmail.com>
2015-07-12 00:41:18 -0500

    FALTA AUTOR

---
+++ CodigosDelAutor.js
@@ -0,0 +1,10 @@
+function codigoAutor() 
+{
+	document.write("<label for=''>Código del Autor</label>");
+	document.write("<select name='codigoautor'>");
+	for(var i=1; i<=100; i++ )
+	{
+		document.write("<option value="+i+">"+i+"</option>");
+	}	
+	document.write("</select>");
+}
