c97831f63c4b8e3526f2d04a104a58f276f7ce03
Luis Alberto <wrtuv@hotmail.com>
2015-07-12 23:15:36 -0500

    ingresar alumno

--- IngresarAlumnoPHP.php
+++ IngresarAlumnoPHP.php
@@ -5,47 +5,47 @@
 <BODY>
 <?php
 
-$codigoalumno = $_POST['codigoalumno'];
-$DNIAlumno = $_POST['DNIAlumno'];
-$NombreAlumno = $_POST['NombreAlumno'];
-$ApellidosAlumno = $_POST['ApellidosAlumno'];
-$CelularAlumno = $_POST['CelularAlumno'];
-$CorreoAlumno = $_POST['CorreoAlumno'];
-$EstadoAlumno = $_POST['EstadoAlumno'];
+	$codigoalumno = $_POST['codigoalumno'];
+	$DNIAlumno = $_POST['DNIAlumno'];
+	$NombreAlumno = $_POST['NombreAlumno'];
+	$ApellidosAlumno = $_POST['ApellidosAlumno'];
+	$CelularAlumno = $_POST['CelularAlumno'];
+	$CorreoAlumno = $_POST['CorreoAlumno'];
+	$EstadoAlumno = $_POST['EstadoAlumno'];
 
 
 
-echo "".$codigoalumno."";
-echo "".$DNIAlumno."";
-echo "".$NombreAlumno."";
-echo "".$ApellidosAlumno."";
-echo "".$CelularAlumno."";
-echo "".$CorreoAlumno."";
-echo "".$EstadoAlumno."";
+	echo "".$codigoalumno."";
+	echo "".$DNIAlumno."";
+	echo "".$NombreAlumno."";
+	echo "".$ApellidosAlumno."";
+	echo "".$CelularAlumno."";
+	echo "".$CorreoAlumno."";
+	echo "".$EstadoAlumno."";
 
-$conectar = mysql_connect("localhost","root","TechKeyWorld2015") or die 
-            ("no se conecto");
-if ($conectar)
-      {
-            echo ("Se ha conectado correctamente");
-      }
-mysql_select_db("bdbibliotecatkw",$conectar);
+	$conectar = mysql_connect("localhost","root","TechKeyWorld2015") or die 
+	            ("no se conecto");
+	if ($conectar)
+	      {
+	            echo ("Se ha conectado correctamente");
+	      }
+	mysql_select_db("bdbibliotecatkw",$conectar);
 
-$consulta="INSERT INTO TAlumno ( CodigoAlumno,DNIAlumno, NombreAlumno,ApellidosAlumno,CelularAlumno,CorreoAlumno,EstadoAlumno)
-      values ('$codigoalumno','$DNIAlumno','$NombreAlumno','$ApellidosAlumno','$CelularAlumno','$CorreoAlumno','$EstadoAlumno')";
+	$consulta="INSERT INTO TAlumno ( CodigoAlumno,DNIAlumno, NombreAlumno,ApellidosAlumno,CelularAlumno,CorreoAlumno,EstadoAlumno)
+	      values ('$codigoalumno','$DNIAlumno','$NombreAlumno','$ApellidosAlumno','$CelularAlumno','$CorreoAlumno','$EstadoAlumno')";
 
-$hacerconsulta=mysql_query($consulta,$conectar);
-if($hacerconsulta)
-{
-      echo ("Se ha grabado el registro");
-}
-else
-{
-      echo ("No se ha podido grabar el registro");
-}
+	$hacerconsulta=mysql_query($consulta,$conectar);
+	if($hacerconsulta)
+	{
+	      echo ("Se ha grabado el registro");
+	}
+	else
+	{
+	      echo ("No se ha podido grabar el registro");
+	}
 
 
-?>
+	?>
 
 
 </BODY>
